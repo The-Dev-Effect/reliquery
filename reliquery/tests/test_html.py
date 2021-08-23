0445f41595a4a2759feb536cccec50ecb34b9cd0
welsh <welsh@pop-os.localdomain>
2021-08-23 16:23:01 -0400

    naming clean up

--- reliquery/tests/test_html.py
+++ reliquery/tests/test_html.py
@@ -13,7 +13,7 @@ def test_list_html_file_when_add_html(test_storage):
 
     rq = relic.Relic(name="test", relic_type="test", storage=test_storage)
 
-    rq.add_html("test html.html", os.path.join(os.path.dirname(__file__), "test.html"))
+    rq.add_html("test-html.html", os.path.join(os.path.dirname(__file__), "test.html"))
     html_list = rq.list_html()
     assert len(html_list) > 0
 
@@ -22,9 +22,9 @@ def test_html_file_given_file_name(test_storage):
 
     rq = relic.Relic(name="test", relic_type="test", storage=test_storage)
 
-    rq.add_html("test html.html", os.path.join(os.path.dirname(__file__), "test.html"))
+    rq.add_html("test-html.html", os.path.join(os.path.dirname(__file__), "test.html"))
 
-    html = rq.get_html("test html.html")
+    html = rq.get_html("test-html.html")
     assert html != None
     assert html.startswith("<div>")
 
@@ -33,7 +33,7 @@ def test_list_html_files(test_storage):
 
     rq = relic.Relic(name="element-bucket", relic_type="test", storage=test_storage)
 
-    rq.add_html("test html.html", os.path.join(os.path.dirname(__file__), "test.html"))
     rq.add_html("test-html.html", os.path.join(os.path.dirname(__file__), "test.html"))
+    rq.add_html("test-html2.html", os.path.join(os.path.dirname(__file__), "test.html"))
     html_list = rq.list_html()
     assert len(html_list) == 2
