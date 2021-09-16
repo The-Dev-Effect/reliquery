eec571affcae1aae8a6a0d615841fab79a2a03db
welsh <welsh@pop-os.localdomain>
2021-09-16 09:21:39 -0400

    added tags and descriptors

---
+++ reliquery/tests/test_tags.py
@@ -0,0 +1,33 @@
+import pytest
+import os
+from .. import Relic
+from reliquery.storage import FileStorage
+
+
+@pytest.fixture
+def test_storage(tmp_path):
+    return FileStorage(str(tmp_path), name="test-tag-storage")
+
+
+def test_list_tags_given_key_value_pair(test_storage):
+    rq = Relic(name="test", relic_type="test", storage=test_storage)
+
+    rq.add_tag({"color": "red"})
+
+    assert len(rq.metadata_db.get_by_key_value(key="color", value="red")) > 0
+    tag_list = rq.list_tags()
+    assert len(tag_list) > 0
+    assert tag_list[0]["tags"]["color"] == "red"
+
+
+def test_list_text_files(test_storage):
+
+    rq = Relic(name="test", relic_type="test", storage=test_storage)
+
+    rq.add_tag({"color": "red", "day": "monday"})
+    rq.add_tag({"reseacher": "newton"})
+    assert (
+        len(rq.metadata_db.get_all_tags_from_relic("test", "test", test_storage.name))
+        == 3
+    )
+    assert len(rq.list_tags()) == 3
