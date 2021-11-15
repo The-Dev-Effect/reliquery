56671717f9bb675ec83901ba35e47bc52dd90737
Jacqueline <jacqueline@Ryans-MacBook-Pro.local>
2021-11-15 17:02:33 -0500

    Name fixes

--- reliquery/tests/test_file.py
+++ reliquery/tests/test_file.py
@@ -14,7 +14,7 @@ def test_list_file_when_add_file(test_storage):
 
     assert len(rq.list_files()) == 0
     test_file = os.path.join(os.path.dirname(__file__), "ideal-engineer.png")
-    rq.add_files_from_path("TestFile", test_file)
+    rq.add_file_from_path("TestFile", test_file)
 
     file_list = rq.list_files()
     assert len(file_list) == 1
@@ -24,11 +24,11 @@ def test_save_files_to_path(test_storage, tmp_path):
     rq = Relic(name="test", relic_type="test", storage=test_storage)
 
     test_file = os.path.join(os.path.dirname(__file__), "ideal-engineer.png")
-    rq.add_files_from_path("Image", test_file)
+    rq.add_file_from_path("Image", test_file)
     path_to_save = os.path.join(tmp_path, "testsaved.png")
 
     assert os.path.exists(path_to_save) is False
-    rq.save_files_to_path("Image", path_to_save)
+    rq.save_file_to_path("Image", path_to_save)
 
     assert os.path.exists(path_to_save)
 
@@ -37,8 +37,8 @@ def test_get_files_given_file_name(test_storage):
     rq = Relic(name="test", relic_type="test", storage=test_storage)
 
     test_file = os.path.join(os.path.dirname(__file__), "ideal-engineer.png")
-    rq.add_files_from_path("TestFile", test_file)
+    rq.add_file_from_path("TestFile", test_file)
 
-    stream = rq.get_files("TestFile")
+    stream = rq.get_file("TestFile")
     assert len(stream.read()) > 0
     assert stream.name.split("/")[-1] == "TestFile"
