2b8245c19e654fc800b4a2216f987b447820970c
Jacqueline <jacqueline@Ryans-MacBook-Pro.local>
2021-12-17 13:52:09 -0500

    Dropbox

--- reliquery/tests/test_storage.py
+++ reliquery/tests/test_storage.py
@@ -9,6 +9,7 @@ from reliquery.storage import (
     get_storage_by_name,
     FileStorage,
     get_all_available_storages,
+    DropboxStorage,
 )
 
 raw_config = """
@@ -29,10 +30,20 @@ raw_config = """
                 "args": {
                 }
             }
+        },
+        "dropbox": {
+            "storage": {
+                "type": "Dropbox",
+                "args": {
+                    "prefix": "rel",
+                    "access_token":""
+                }
+            }
         }
     }"""
 
 
+# S3 Tests
 def test_use_s3_when_getting_storage_with_config_having_s3_type(tmpdir):
     reliquery_dir = os.path.join(tmpdir, "reliquery")
     os.makedirs(reliquery_dir)
@@ -112,6 +123,15 @@ def test_use_demo_s3_storage_when_getting_storage_with_config_having_demo_name(t
     assert "rel" == storage.prefix
 
 
+@mock.patch.dict(os.environ, {"RELIQUERY_CONFIG": raw_config})
+def test_use_s3_storage_when_passing_s3_config_in_environment_as_variable(tmpdir):
+
+    storage = get_storage_by_name("s3", tmpdir)
+
+    assert isinstance(storage, S3Storage)
+
+
+# FileStorageTests
 def test_use_file_storage_when_getting_default_storage_without_config(tmpdir):
     storage = get_storage_by_name("default", tmpdir)
     assert isinstance(storage, FileStorage)
@@ -138,14 +158,34 @@ def test_use_file_storage_when_getting_storage_with_config_having_file_type(
     assert isinstance(storage, FileStorage)
 
 
-@mock.patch.dict(os.environ, {"RELIQUERY_CONFIG": raw_config})
-def test_use_s3_storage_when_passing_s3_config_in_environment_as_variable(tmpdir):
+# DropboxTests
+@mock.patch("reliquery.storage.dropbox.Dropbox")
+def test_use_dropbox_when_getting_storage_with_config_having_dropbox_type(tmpdir):
+    reliquery_dir = os.path.join(tmpdir, "reliquery")
+    os.makedirs(reliquery_dir)
+    config_path = os.path.join(reliquery_dir, "config")
 
-    storage = get_storage_by_name("s3", tmpdir)
+    with open(config_path, mode="w+") as config_file:
+        config = {
+            "Dropbox": {
+                "storage": {
+                    "type": "Dropbox",
+                    "args": {
+                        "prefix": "rel",
+                        "access_token": "",
+                    },
+                }
+            }
+        }
 
-    assert isinstance(storage, S3Storage)
+        config_file.write(json.dumps(config, indent=4))
+
+    storage = get_storage_by_name("Dropbox", tmpdir)
+    assert isinstance(storage, DropboxStorage)
+    assert "/rel" == storage.prefix
 
 
+# S3 and FileStorage Tests
 def test_error_when_getting_default_storage_with_config_having_unknown_type(tmpdir):
     reliquery_dir = os.path.join(tmpdir, "reliquery")
     os.makedirs(reliquery_dir)
@@ -170,12 +210,15 @@ def test_availible_storages_returns_default_storage(tmpdir):
 
 
 @mock.patch.dict(os.environ, {"RELIQUERY_CONFIG": raw_config})
+@mock.patch("reliquery.storage.dropbox.Dropbox")
 def test_different_storage_types_given_env_config(tmpdir):
     storages = get_all_available_storages(tmpdir)
 
-    assert len(storages) == 2
+    assert len(storages) == 3
     for stor in storages:
-        if type(stor) == FileStorage:
+        if isinstance(stor, FileStorage):
             assert stor.name == "file"
-        elif type(stor) == S3Storage:
+        elif isinstance(stor, S3Storage):
             assert stor.name == "s3"
+        elif isinstance(stor, DropboxStorage):
+            assert stor.name == "dropbox"
